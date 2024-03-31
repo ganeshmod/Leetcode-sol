75716fa9c2b051c7eafa7052c6416b0a661c0188
Ganesh Modanwal <151774019+ganeshmod@users.noreply.github.com>
2024-03-31 23:38:10 +0530

    Time: 3 ms (100%), Space: 56.4 MB (50%) - LeetHub

---
+++ 3374-count-alternating-subarrays/3374-count-alternating-subarrays.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long countAlternatingSubarrays(int[] nums) {
+        long count=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j+1<n && nums[j]!=nums[j+1]){
+                j++;
+            }
+            long len=j-i+1;
+            count+=(len*(len+1))/2;
+            i=j;
+        }
+        return count;
+    }
+}
