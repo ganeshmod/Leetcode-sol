5e2a1fd8a6f0f484b9aca4fae9be48d97209f983
Ganesh Modanwal <151774019+ganeshmod@users.noreply.github.com>
2024-09-12 17:10:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
-static bool comp(vector<int> &num1,vector<int> &num2)
-{
-    return num1[0]<num2[0];
-}
     vector<vector<int>> merge(vector<vector<int>>& intr) {
         vector<vector<int>> ans;
-        sort(intr.begin(),intr.end(),comp);
         int initial=intr[0][0];
         int final=intr[0][1];
         for(int i=1;i<intr.size();i++)
