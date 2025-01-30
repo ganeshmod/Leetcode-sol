963b9759ced8436da3f998de4afe519f0cbb6737
Ganesh Modanwal <151774019+ganeshmod@users.noreply.github.com>
2025-01-30 19:14:00 +0530

    Time: 11 ms (42.9%), Space: 48.5 MB (58.1%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,24 +1,23 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        
         int n=g.length;
         int m=s.length;
-        Arrays.sort(g);
         Arrays.sort(s);
+        Arrays.sort(g);
         int l=0;
         int r=0;
-        //greed pr l hai 
-        // s pr r hai
-        while(l<n && r<m)
-{
-    if(s[r]>=g[l]){
-        r++;
-        l++;
-    }
-    else if((s[r]<g[l])){
-        r++;
-    }
-
-}  
-return l;      
+        int count=0;
+        while(l<n  && r<m){
+            if(g[l]<=s[r]){
+            r++;
+            l++;
+            count++;
+            }
+            else{
+            r++;
+            }
+        }
+        return count;
     }
 }
