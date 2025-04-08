db80ffbbaf7f894600108addc3bcd91efbe4e31d
Ganesh Modanwal <151774019+ganeshmod@users.noreply.github.com>
2025-04-08 22:47:13 +0530

    Time: 20 ms (23.31%), Space: 55.3 MB (89.72%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       
-        int[] arr=new int[(int)1e5+1];
-       
-        for(int i=0;i<nums.length;i++){
-            arr[nums[i]]++;
-        }
-        for(int i:arr){
-            if(i>=2){
-                return true ;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
             }
         }
         return false;
